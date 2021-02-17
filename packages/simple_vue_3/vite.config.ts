39d7d76230f102fcf86923f51276e509b492508c
vvsubash <subash.venkat007@gmail.com>
2021-02-17 07:47:18 +0530

    ef

---
+++ packages/simple_vue_3/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import voie from 'vite-plugin-voie';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(),voie()]
+})
